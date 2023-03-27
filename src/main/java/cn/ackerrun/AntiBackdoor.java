296f1851d3e04eda3a0d7e72d36e18e613e70ca4
AckerRun1337 <cat@nachoneko.us>
2023-03-27 11:54:24 +0800

    update antibackdoor Add more detection backdoors

--- src/main/java/cn/ackerrun/AntiBackdoor.java
+++ src/main/java/cn/ackerrun/AntiBackdoor.java
@@ -5,6 +5,7 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.server.PluginEnableEvent;
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -27,10 +28,59 @@ public class AntiBackdoor extends JavaPlugin implements Listener {
         String pluginFilePath = pluginFile.getAbsolutePath();
         String[] authors = description.getAuthors().toArray(new String[0]);
 
+        // 检查插件是否为后门插件
+        try {
+            if (BackdoorDetector.isBackdoorPlugin(pluginName, pluginFilePath, authors)) {
+                getLogger().warning("Detected backdoor plugin " + pluginName + ", disabling...");
+                event.getPlugin().getPluginLoader().disablePlugin(event.getPlugin());
+                return;
+            }
+        } catch (IOException e) {
+            getLogger().warning("Error reading plugin file: " + pluginFilePath);
+            e.printStackTrace();
+            return;
+        }
 
-        if (BackdoorDetector.isBackdoorPlugin(pluginName, pluginFilePath, authors)) {
-            getLogger().warning("Detected backdoor plugin " + pluginName + ", disabling...");
-            event.getPlugin().getPluginLoader().disablePlugin(event.getPlugin());
+        // 检查插件的类是否位于默认包中
+        for (Class<?> clazz : event.getPlugin().getClass().getClasses()) {
+            if (clazz.getPackage() == null) {
+                getLogger().warning("Plugin " + pluginName + " contains a class in the default package, disabling...");
+                event.getPlugin().getPluginLoader().disablePlugin(event.getPlugin());
+                return;
+            }
+        }
+
+        // 检查插件是否依赖其他插件
+        for (String dependentName : description.getDepend()) {
+            Plugin dependentPlugin = getServer().getPluginManager().getPlugin(dependentName);
+
+            if (dependentPlugin != null) {
+                PluginDescriptionFile dependentDescription = dependentPlugin.getDescription();
+
+                // 检查依赖的插件是否为已知的后门插件
+                if (BackdoorDetector.isKnownBackdoorPlugin(dependentName)) {
+                    getLogger().warning("Plugin " + pluginName + " depends on a known backdoor plugin " + dependentName + ", disabling...");
+                    event.getPlugin().getPluginLoader().disablePlugin(event.getPlugin());
+                    return;
+                }
+            }
+        }
+
+        // 检查插件是否包含敏感API调用
+        for (StackTraceElement element : Thread.currentThread().getStackTrace()) {
+            String className = element.getClassName();
+            String methodName = element.getMethodName();
+
+
+            if (className.equals("java.lang.ClassLoader") && methodName.equals("loadClass")) {
+                getLogger().warning("Plugin " + pluginName + " calls sensitive API java.lang.ClassLoader.loadClass, disabling...");
+                event.getPlugin().getPluginLoader().disablePlugin(event.getPlugin());
+                return;
+            } else if (className.startsWith("org.reflections.")) {
+                getLogger().warning("Plugin " + pluginName + " uses the reflections library, disabling...");
+                event.getPlugin().getPluginLoader().disablePlugin(event.getPlugin());
+                return;
+            }
         }
     }
 
--- src/main/java/cn/ackerrun/Detector/BackdoorDetector.java
+++ src/main/java/cn/ackerrun/Detector/BackdoorDetector.java
@@ -1,57 +1,130 @@
 package cn.ackerrun.Detector;
 
-import java.util.Arrays;
-import java.util.List;
+import javax.xml.bind.DatatypeConverter;
+import java.util.*;
 
 /***********************
  *   @Author: Rain
  *   @Date: 2023/3/27
  * **********************
  */
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.security.MessageDigest;
+import java.util.regex.Pattern;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
+
 public class BackdoorDetector {
 
-   private static final List<String> BLACKLISTED_PLUGIN_NAMES = Arrays.asList(
-           "BackdoorPlugin", "EvilPlugin", "HackerPlugin"
-   );
+   private static final Set<String> KNOWN_BACKDOOR_PLUGINS = new HashSet<>();
+   private static final Pattern CLASS_NAME_PATTERN = Pattern.compile("^[a-zA-Z][a-zA-Z0-9_]*(\\$[a-zA-Z][a-zA-Z0-9_]*)*$");
 
-   private static final List<String> BLACKLISTED_AUTHORS = Arrays.asList(
-           "hacker123", "evilguy", "backdoorinc"
-   );
-
-   private BackdoorDetector() {
+   static {
+      // 添加已知的后门插件名称
+      KNOWN_BACKDOOR_PLUGINS.add("EvilPlugin");
    }
 
-   public static boolean isBackdoorPlugin(String pluginName, String fileName, String[] authors) {
-      return isInBlacklist(pluginName)
-              || containsKeyword(fileName, "backdoor")
-              || containsKeyword(fileName, "hack")
-              || containsAnyKeyword(authors, BLACKLISTED_AUTHORS);
-   }
+   /**
+    * 检查指定的插件是否为后门插件。
+    *
+    * @param pluginName 插件名称
+    * @param filePath   插件文件路径
+    * @param authors    插件作者列表
+    * @return true 如果该插件被检测到为后门插件；false 反之。
+    * @throws IOException 如果在读取文件时出现错误。
+    */
+   public static boolean isBackdoorPlugin(String pluginName, String filePath, String[] authors) throws IOException {
+      if (KNOWN_BACKDOOR_PLUGINS.contains(pluginName)) {
+         return true;
+      }
 
-   private static boolean isInBlacklist(String pluginName) {
-      return BLACKLISTED_PLUGIN_NAMES.contains(pluginName);
-   }
+      ZipFile zip = new ZipFile(filePath);
+      Set<String> classNames = new HashSet<>();
 
-   private static boolean containsKeyword(String string, String keyword) {
-      return string.toLowerCase().contains(keyword.toLowerCase());
-   }
+      try {
+         for (ZipEntry entry : Collections.list(zip.entries())) {
+            String name = entry.getName();
 
-   private static boolean containsAnyKeyword(String[] strings, List<String> keywords) {
-      for (String string : strings) {
-         if (containsAnyKeyword(string, keywords)) {
+            // 如果不是.class文件，则跳过
+            if (!name.endsWith(".class")) {
+               continue;
+            }
+
+            // 获取类名，并进行格式验证
+            String className = name.replace('/', '.').substring(0, name.length() - 6);
+            if (!CLASS_NAME_PATTERN.matcher(className).matches()) {
+               continue;
+            }
+
+            classNames.add(className);
+         }
+      } finally {
+         zip.close();
+      }
+
+      // 检查作者是否是匿名
+      for (String author : authors) {
+         if (author.equalsIgnoreCase("anonymous")) {
             return true;
          }
       }
+
+      // 检查是否包含敏感类或代码
+      for (String className : classNames) {
+         if (className.contains("$") || className.startsWith("org.bukkit.plugin.java.") || className.startsWith("net.md_5.")) {
+            continue;
+         }
+
+         try {
+            Class<?> clazz = Class.forName(className);
+
+            if (clazz.isAnnotationPresent(Deprecated.class) ||
+                    clazz.getName().contains("sun.reflect.")) {
+               return true;
+            }
+
+            if (clazz.getPackage() == null) {
+               return true;
+            }
+
+            // 检查字节码是否包含特定的字节数组
+            byte[] bytes = Files.readAllBytes(Paths.get(filePath));
+            MessageDigest md = MessageDigest.getInstance("SHA-256");
+            byte[] digest = md.digest(bytes);
+            if (containsMagicCode(digest)) {
+               return true;
+            }
+         } catch (ClassNotFoundException e) {
+            // 如果无法加载类，直接跳过
+         } catch (Exception e) {
+            e.printStackTrace();
+         }
+      }
+
       return false;
    }
 
-   private static boolean containsAnyKeyword(String string, List<String> keywords) {
-      for (String keyword : keywords) {
-         if (containsKeyword(string, keyword)) {
-            return true;
-         }
-      }
-      return false;
+   /**
+    * 判断指定的插件名称是否为已知的后门插件。
+    *
+    * @param pluginName 插件名称
+    * @return true 如果该插件名称为后门插件；false 反之。
+    */
+   public static boolean isKnownBackdoorPlugin(String pluginName) {
+      return KNOWN_BACKDOOR_PLUGINS.contains(pluginName);
+   }
+
+   /**
+    * 判断指定的字节数组是否包含特定的字节数组。
+    *
+    * @param bytes 要检查的字节数组
+    * @return true 如果该字节数组包含特定的字节数组；false 反之。
+    */
+   private static boolean containsMagicCode(byte[] bytes) {
+      String hex = DatatypeConverter.printHexBinary(bytes);
+      return hex.contains("4A6F794D63212121") || hex.contains("4E65726F707820");
    }
 
 }
